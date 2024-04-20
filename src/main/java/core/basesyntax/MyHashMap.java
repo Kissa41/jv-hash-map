e8fd52ed39770bbe909771e552ddd92572115121
Kissa41 <nastyagoncharenko5@gmail.com>
2024-04-20 17:51:56 +0200

    Fixing hashmap implementation

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,11 +1,10 @@
 package core.basesyntax;
 
 import java.util.Objects;
-import java.lang.reflect.Field;
 
-public class MyHashMap<K, V> implements MyMap<K, V>, Cloneable {
+public class MyHashMap<K, V> implements MyMap<K, V> {
 
-    static class Node<V, K>{
+    static class Node<K, V>{
         final int hash;
         final K key;
         V value;
@@ -20,185 +19,97 @@ public class MyHashMap<K, V> implements MyMap<K, V>, Cloneable {
 
     }
 
-    private int totalSize = 16;
-    final static float loadFactor = 0.75f;
-    Node[] table = new Node[getTotalSize()];
+    final private static int DEFAULT_CAPACITY = 16;
+    final private static float LOAD_FACTOR = 0.75f;
+    final private static int ARRAY_RESIZE_INCREMENT = 2;
+    private int currentSize = 0;
+    Node<K, V>[] table = new Node[DEFAULT_CAPACITY];
 
-
-    public void setTotalSize(int totalSize) {
-        this.totalSize = totalSize;
-    }
-
-    public int getTotalSize() {
-        return totalSize;
-    }
-
-    public final int hash(K key, V value) {
-        int res = Objects.hash(key, value);
+    public final int calculateIndex(K key) {
+        int res = Objects.hash(key);
         if(res < 0) res*=-1;
-        return res;
-    }
-
-    public static boolean compareObjects(Object obj1, Object obj2) {
-        if (obj1 == obj2)
-            return true;
-        if (obj1 == null || obj2 == null || obj1.getClass() != obj2.getClass())
-            return false;
-
-        Field[] fields = obj1.getClass().getDeclaredFields();
-
-        for (Field field : fields) {
-            try {
-                field.setAccessible(true);
-                Object value1 = field.get(obj1);
-                Object value2 = field.get(obj2);
-
-                if (!Objects.equals(value1, value2))
-                    return false;
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return true;
+        return res % table.length;
     }
 
     @Override
     public void put(K key, V value) {
 
         // Resizing
-        int threshold = (int) (loadFactor * getTotalSize());
-        int currentSize = this.getSize();
+        int threshold = (int) (LOAD_FACTOR * table.length);
         if (currentSize + 1 > threshold){
-            this.resize();
+            resize();
         }
 
-        // Getting of element index
-        int h = this.hash(key, value);
-        int index = h % getTotalSize();
+        int index = calculateIndex(key);
+        Node<K, V> node = new Node(Objects.hash(key), key, value, null);
 
-        // If the key exists, we change the value
-        for (int i = 0; i < table.length; i++){
-            if (table[i] instanceof Node) {
-                if(table[i].key instanceof Object && compareObjects(table[i].key, key)){
-                    table[i].value = value;
-                    return;
-                }
-                else if(table[i].key == key){
-                    table[i].value = value;
-                    return;
-                }
-            }
+        // If bucket is empty we put a new Node in it
+        if (table[index]==null){
+            table[index] = node;
         }
-
-        // If the key doesn't exist, we create a new Node
-        if (this.table[index] == null) {
-            Node node = new Node(h, key, value, null);
-            this.table[index] = node;
-        }
+        // If bucket isn't empty we check all linked list and
+        // select the appropriate bucket
         else{
-            Node oldNode = table[index];
-            if (oldNode.key!=key && oldNode.value!=value){
-                Node temp1 = oldNode;
-                Node temp2 = oldNode.next;
-                if (temp2 == null) temp1.next = new Node(h, key, value, null);
-                else{
-                    while (temp2!=null){
-                        temp1 = temp1.next;
-                        temp2 = temp2.next;
-                    }
-                    temp1.next = new Node(h, key, value, null);
+            Node<K, V> temp = table[index];
+            while (temp.next != null){
+                if (Objects.equals(temp.key, key)) {
+                    temp.value = value;
+                    return;
                 }
+                temp = temp.next;
             }
+            if (Objects.equals(temp.key, key)) {
+                temp.value = value;
+                return;
+            }
+            temp.next = node;
         }
+
+        currentSize+=1;
+
     }
     @Override
     public V getValue(K key) {
 
         V result = null;
 
-        for (int i = 0; i < table.length; i++) {
+        int index = calculateIndex(key);
+        Node<K, V> temp = table[index];
 
-            Node node = table[i];
-
-            // If in this bucket there is only one Node we check it
-            if (node instanceof Node) {
-                if (node.next == null) {
-                    if (node.key instanceof Object && compareObjects(node.key, key)) {
-                        result = (V) node.value;
-                    } else if (node.key == key) {
-                        result = (V) node.value;
-                    }
-                }
-
-                // If in this bucket there is more than one Node we check all of them
-                else {
-                    while (node != null) {
-                        if (node.key instanceof Object && compareObjects(node.key, key)) {
-                            result = (V) node.value;
-                        } else if (node.key == key) {
-                            result = (V) node.value;
-                        }
-                        node = node.next;
-                    }
-                }
-
-            }
-
+        while (temp!=null){
+            if (Objects.equals(temp.key, key)) result = temp.value;
+            temp = temp.next;
         }
+
         return result;
     }
 
     @Override
     public int getSize() {
-
-        int size = 0;
-
-        for (int i = 0; i < table.length; i++){
-
-            Node node = table[i];
-
-            if (node instanceof Node) {
-                size++;
-            }
-
-            if (node instanceof Node && node.next != null) {
-
-                Node temp = node.next;
-
-                while (temp != null) {
-                    size++;
-                    temp = temp.next;
-                }
-            }
-
-        }
-        return size;
+        return currentSize;
     }
 
     private void resize(){
 
-        // Double the size
-        setTotalSize(getTotalSize()*2);
-
-        // Re-create the array "table"
-        Node[] oldTable = table;
-        Node[] newTable = new Node[getTotalSize()];
-        table = newTable;
+        // Re-create the array "table" with doubled capacity
+        Node<K, V>[] oldTable = table;
+        table = new Node[table.length * ARRAY_RESIZE_INCREMENT];
 
         // Re-locate all Nodes by new indices
-        for (int i = 0; i < oldTable.length; i++){
+        for (Node<K, V> node : oldTable) {
 
-            Node node = oldTable[i];
+            if (node != null) {
+                put(node.key, node.value);
+                currentSize -= 1;
+            }
 
-            if (node!=null) this.put((K)node.key, (V)node.value);
+            if (node != null && node.next != null) {
 
-            if (node!=null && node.next != null) {
-
-                Node temp = node.next;
+                Node<K, V> temp = node.next;
 
                 while (temp != null) {
-                    this.put((K)temp.key, (V)temp.value);
+                    put(temp.key, temp.value);
+                    currentSize -= 1;
                     temp = temp.next;
                 }
             }
